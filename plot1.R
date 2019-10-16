083fc0b35165a787f7fd47eb2bf4a244e1260c5b
mikeige <mikeige@outlook.com>
2019-10-16 14:26:17 -0400

    Plots

---
+++ plot1.R
@@ -0,0 +1,12 @@
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "HHPower.zip", method = "curl")
+  
+  unzip("HHPower.zip")
+  
+data <- read.table("household_power_consumption.txt", header= TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+
+subsetdata <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+
+globalActivePower <- as.numeric(subsetdata$Global_active_power)
+
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
---
+++ plot2.R
@@ -0,0 +1,13 @@
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "HHPower.zip", method = "curl")
+  
+  unzip("HHPower.zip")
+  
+data <- read.table("household_power_consumption.txt", header= TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+
+subsetdata <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+
+globalActivePower <- as.numeric(subsetdata$Global_active_power)
+
+datetime <- strptime(paste(subsetdata$Date, subsetdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
